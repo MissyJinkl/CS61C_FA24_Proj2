f0fc1c1b9d141d550f5ec20f4d3f06a7b132c91c
Missy Jin <missy_jin@berkeley.edu>
2024-09-20 13:46:36 -0700

    ex_2

--- src/relu.s
+++ src/relu.s
@@ -16,18 +16,17 @@ relu:
     # Prologue
     li t0, 1
     blt a1, t0, exceptions
-    li t1, 1 # i = 1
+    li t1, 0 # i = 0
     mv t2, a0 # t2 is the pointer
 
 loop_start:
-    blt a1, t1, loop_end
+    bge t1, a1, loop_end
     lw t4, 0(t2)
     bge t4, x0, loop_continue
     sw x0, 0(t2)
-    j loop_continue
     
 loop_continue:
-    slli t2, t2, 2
+    addi t2, t2, 4
     addi t1, t1, 1
     j loop_start
 
